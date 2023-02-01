c4fbe069e9910e1bc13aa61a01b95e444716fa26
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-01-31 19:50:50 -0800

    make run.sh generic

--- homework-assignment-1/run.sh
+++ homework-assignment-1/run.sh
@@ -1,13 +1,10 @@
-# check if all arguments are received
-if [ "$#" -ne 3 ]; then
-    echo "Illegal number of parameters"
-    exit 1;
-fi
-
 # set correct env vars
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 export PYSPARK_PYTHON=python3.6
 
 # execute the script
-/opt/spark/spark-3.1.2-bin-hadoop3.2/bin/spark-submit --executor-memory 4G --driver-memory 4G "$1" "$2" "$3"
+/opt/spark/spark-3.1.2-bin-hadoop3.2/bin/spark-submit --executor-memory 4G --driver-memory 4G "$@"
+
+# pass a range of arguments
+# https://stackoverflow.com/questions/3811345/how-to-pass-all-arguments-passed-to-my-bash-script-to-a-function-of-mine
