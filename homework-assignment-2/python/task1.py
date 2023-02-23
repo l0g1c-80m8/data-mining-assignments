1768168f6eb101af5299558e7cdde661924903a6
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-02-22 22:19:56 -0800

    refactor - complete pass one of SON

--- homework-assignment-2/python/task1.py
+++ homework-assignment-2/python/task1.py
@@ -4,127 +4,164 @@ Task 1
 
 Read data from the input file and implement SON algorithm with PCY algorithm to fund frequent item-sets.
 """
-
 import os
 import sys
 
-from collections import defaultdict
+from collections import defaultdict, Counter
 from datetime import datetime, timedelta
+from itertools import chain, combinations, groupby
 from operator import add
 from pyspark import SparkConf, SparkContext
 
-os.environ['PYSPARK_PYTHON'] = sys.executable
-os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
-# first clock measurement
-start_time_stamp = datetime.now()
+def parse_args():
+    if len(sys.argv) < 4:
+        # expected arguments: script path, dataset path, output file path
+        print('ERR: Expected three arguments: (case number, support, input file path, output file path).')
+        exit(1)
 
-# ------------------------------------------------ start ------------------------------------------------ #
+    # read program arguments
+    params['app_name'] = 'hw2-task1'
+    params['case_number'] = int(sys.argv[1])
+    params['support'] = int(sys.argv[2])
+    params['in_file'] = sys.argv[3]
+    params['out_file'] = sys.argv[4]
+    return params
 
-if len(sys.argv) < 4:
-    # expected arguments: script path, dataset path, output file path
-    print('ERR: Expected three arguments: (case number, support, input file path, output file path).')
-    exit(1)
 
-# read program arguments
-case_number = int(sys.argv[1])
-support = int(sys.argv[2])
-input_file_path = sys.argv[3]
-output_file_path = sys.argv[4]
+def construct_rdd():
+    data_rdd = sc.textFile(params['in_file']) \
+        .map(lambda line: line.split(',')) \
+        .filter(lambda user_business_pair: user_business_pair != ['user_id', 'business_id'])
 
-# set spark app config
-sc = SparkContext(conf=SparkConf().setAppName("hw1-task2").setMaster("local[*]"))
-sc.setLogLevel('ERROR')
-
-'''
-SON Algorithm
-
-First pass:
-- a. Divide the data into chunks (done earlier) (map task).
-- b. Treat each chunk as a sample and run the A-Priori algorithm with threshold ps (map task) 
-     (p => fraction in the chunk, s => support).
-- c. Collect all item-sets found frequent in step b in any of the chunk.
-     These form the candidate item-sets for the second pass (filter - map task). 
-
-Second pass:
-- d. Go through the full data-set and count the occurrence of candidate item-sets from c. of first pass and
-     count the truly frequent item-sets in the whole dataset (remove false negatives) (reduce task).
-'''
-
-# [SON a.] read the data and construct a spark RDD object with appropriate structure for processing
-dataRDD = sc.textFile(input_file_path) \
-    .map(lambda line: line.split(',')) \
-    .filter(lambda user_business_pair: user_business_pair != ['user_id', 'business_id'])
-
-# [SON a.] convert rdd to tuple and set the key based on the case number
-if case_number == 1:
-    '''
-    Case 1
-    Using users as basket, group businesses into them as items.
-    Each basket is represented by a user id and is a set of business ids as items.
-    '''
-    dataRDD = dataRDD.map(lambda two_list: tuple(two_list))
-elif case_number == 2:
-    '''
+    if params['case_number'] == 1:
+        '''
+        Case 1
+        Using users as basket, group businesses into them as items.
+        Each basket is represented by a user id and is a set of business ids as items.
+        '''
+        data_rdd = data_rdd.map(lambda two_list: tuple(two_list))
+    elif params['case_number'] == 2:
+        '''
         Case 2
         Using businesses as basket, group users into them as items.
         Each basket is represented by a business id and is a set of user ids as items.
         '''
-    dataRDD = dataRDD.map(lambda two_list: tuple(reversed(two_list)))
-else:
-    # No case for other values
-    print('ERR: Expected case number: to be 0 or 1.')
-    exit(1)
+        data_rdd = data_rdd.map(lambda two_list: tuple(reversed(two_list)))
+    else:
+        # No case for other values
+        print('ERR: Expected case number: to be 0 or 1.')
+        exit(1)
 
-# [SON a.] create baskets based on the id with unique entries (list of [transaction id: set of item ids])
-dataRDD = dataRDD.groupByKey().mapValues(set)
-# [SON a.] get the total transactions in the RDD
-total_transaction_count = dataRDD.count()
+    data_rdd = data_rdd.groupByKey().mapValues(set)
+
+    return data_rdd
 
 
-# [SON b.] define a function to execute the apriori algorithm on an RDD partition (chunk)
+def get_frequents_in_chunk(
+        transactions,
+        chunk_support,
+        chunk_frequent_prev,
+        frequent_item_set_size
+):
+    frequents = list()
+    candidates = combinations(chunk_frequent_prev, 2)
+    if frequent_item_set_size == 2:
+        for candidate in candidates:
+            candidate_count = 0
+            for transaction in transactions:
+                if set(candidate).issubset(transaction):
+                    candidate_count += 1
+                if candidate_count >= chunk_support:
+                    frequents.append(candidate)
+                    break
+    else:
+        candidates = filter(
+            lambda candidate_set: len(candidate_set) == frequent_item_set_size,
+            map(lambda candidate_pair: tuple(sorted(set(candidate_pair[0]).union(candidate_pair[1]))), candidates)
+        )
+        for candidate in set(candidates):
+            candidate_count = 0
+            for transaction in transactions:
+                if set(candidate).issubset(transaction):
+                    candidate_count += 1
+                if candidate_count >= chunk_support:
+                    frequents.append(tuple(candidate))
+                    break
+        print(frequents)
+
+    return frequents
+
+
 def apriori(chunk):
-    """
-    The A-Priori algorithm
-    - a. initialize Ck (item-sets and their counts - given as input) for finding item-sets of size k.
-    - b. examine the counts of the items to determine which of them are frequent (count >= support).
-    - c. filter items not satisfying b. and return the output as Lk.
+    # create a list of item sets from transaction chunk/partition
+    transactions = list(map(lambda transaction: transaction[1], chunk))
+    # calculate the adjusted support for this chunk/partition
+    chunk_support = params['support'] * len(transactions) / total_transaction_count
+    # initialize by calculating frequent singletons in the chunk/partition
+    chunk_frequent_current = list(map(
+        lambda item_count: item_count[0],
+        filter(
+            lambda item_count: item_count[1] >= chunk_support,
+            Counter(chain.from_iterable(map(
+                lambda transaction_item_set: list(transaction_item_set),
+                transactions
+            ))).items()
+        )))
+    frequent_item_set_size = 1
 
-    :param chunk: the chunk to be processed
-    :return: the frequent item-sets in this partition (chunk) which are candidates for the global dataset
-    """
-    chunk_transaction_count = 0
-    chunk_item_count_dict = defaultdict(int)
+    # store the frequent item sets in a dictionary keyed by size
+    chunk_frequents_comprehensive = defaultdict(list)
+    chunk_frequents_comprehensive[frequent_item_set_size] = chunk_frequent_current
 
-    for transaction in chunk:
-        chunk_transaction_count += 1
-        for item in transaction[1]:
-            chunk_item_count_dict[item] += 1
+    # iteratively find frequent item sets for bugger sizes from the chunks
+    while True:
+        # increment frequent item set size
+        frequent_item_set_size += 1
+        # get chunks of the bigger size
+        chunk_frequent_current = get_frequents_in_chunk(
+            transactions,
+            chunk_support,
+            chunk_frequent_current,
+            frequent_item_set_size
+        )
+        # quit when no more frequent item sets of the current size are found
+        if len(chunk_frequent_current) == 0:
+            break
+        # add frequent item sets to the dictionary
+        for item in chunk_frequent_current:
+            chunk_frequents_comprehensive[frequent_item_set_size].append(item)
 
-    # [Apriori b.] calculate the fraction of items 'p' and the support 's' for this chunk
-    chunk_fraction = chunk_transaction_count / total_transaction_count
-    chunk_support = chunk_fraction * support
-    # [Apriori b. and c.] filter the frequent item-sets based on the support and return the results
-    chunk_frequent_items = filter(lambda pair: pair[1] > chunk_support, chunk_item_count_dict.items())
-    return map(lambda pair: pair[0], chunk_frequent_items)
+    # return the found frequent item sets of all sizes
+    return list(map(lambda frequents: (frequents[0], frequents[1]), chunk_frequents_comprehensive.items()))
 
 
-# [SON b. and c.] run the apriori algorithm on each chunk with appropriate parameters
-candidate_item_sets = dataRDD.mapPartitions(apriori).distinct(1).collect()
-# [SON d.] count the truly frequent items in the whole data set
-item_sets_counts = dict(dataRDD.map(lambda transaction: list(transaction[1]))
-                        .flatMap(lambda item_list: item_list)
-                        .map(lambda item: (item, 1))
-                        .reduceByKey(add)
-                        .collect())
-frequent_item_sets = filter(lambda item_set: item_sets_counts[item_set] >= support, candidate_item_sets)
+def execute_son():
+    # get the rdd depending on the params
+    transaction_rdd = construct_rdd()
+    # find candidates from individual chunks
+    candidates = transaction_rdd.mapPartitions(apriori)
+    frequents = candidates.collect()
+    print(frequents)
 
-# ------------------------------------------------ end ------------------------------------------------ #
 
-# second clock measurement
-end_time_stamp = datetime.now()
-# calculate the duration and log it to the console
-print('Duration: ', (end_time_stamp - start_time_stamp) / timedelta(microseconds=1))
+if __name__ == '__main__':
+    # set executables
+    os.environ['PYSPARK_PYTHON'] = sys.executable
+    os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
-# exit without errors
-exit(0)
+    # initialize program parameters
+    params = dict()
+    parse_args()
+
+    # create spark context
+    sc = SparkContext(conf=SparkConf().setAppName(params['app_name']).setMaster("local[*]"))
+    sc.setLogLevel('ERROR')
+
+    # global vars
+    rdd = construct_rdd()
+    total_transaction_count = rdd.count()
+    frequent_item_sets = list()
+
+    # run SON with Apriori
+    execute_son()
