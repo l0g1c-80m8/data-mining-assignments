2833f3ca01b9935391c940096906c990e9ca9173
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-02-23 22:20:16 -0800

    fix comment spell error

--- homework-assignment-2/python/task2.py
+++ homework-assignment-2/python/task2.py
@@ -3,7 +3,7 @@ Homework Assignment 2
 Task 2
 
 Read data from the Ta-Feng dataset, pre-process it and implement the SON algorithm
-with Apriori algorithm to fund frequent item-sets.
+with Apriori algorithm to find frequent item-sets.
 """
 
 import os
