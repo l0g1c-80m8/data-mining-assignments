97e4cf3047e62ee58762dbd0b5b8a54a565d4a47
rutvik patel <rutvik.p4t3l@gmail.com>
2023-03-18 18:36:17 -0700

    add script for hw3 task 2 part 1

---
+++ homework-assignment-3/python/task2_1.py
@@ -0,0 +1,53 @@
+"""
+Homework Assignment 3
+Task 2 (Part 1)
+
+An item-based collaborative filtering mechanism to generate recommendation of businesses (items) for users.
+"""
+
+import os
+import sys
+
+from datetime import datetime
+from pyspark import SparkConf, SparkContext
+
+
+def parse_args():
+    if len(sys.argv) < 3:
+        # expected arguments: script path, dataset path, output file path
+        print('ERR: Expected two arguments: (input file path, output file path).')
+        exit(1)
+
+    # read program arguments
+    run_time_params = dict()
+    run_time_params['app_name'] = 'hw3-task2_1'
+    run_time_params['in_file'] = sys.argv[1]
+    run_time_params['test_file'] = sys.argv[2]
+    run_time_params['out_file'] = sys.argv[3]
+    return run_time_params
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    # set executables
+    os.environ['PYSPARK_PYTHON'] = sys.executable
+    os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
+    # initialize program parameters
+    params = parse_args()
+
+    # create spark context
+    sc = SparkContext(conf=SparkConf().setAppName(params['app_name']).setMaster("local[*]"))
+    sc.setLogLevel('ERROR')
+
+    # run min-hashing + locality sensitive hashing
+    start_ts = datetime.now()
+    main()
+    end_ts = datetime.now()
+    print('Duration: ', (end_ts - start_ts).total_seconds())
+
+    # exit without errors
+    exit(0)
