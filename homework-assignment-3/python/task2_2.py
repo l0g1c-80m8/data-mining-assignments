c77624d1675a7eb3712f348c14cbeadd66ca448f
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-03-23 10:29:53 -0700

    add task2_2 script

---
+++ homework-assignment-3/python/task2_2.py
@@ -0,0 +1,115 @@
+import json
+import pandas as pd
+import os
+import sys
+import xgboost as xgb
+
+from collections import defaultdict
+from datetime import datetime
+from pyspark import SparkConf, SparkContext
+
+
+def parse_args():
+    if len(sys.argv) < 3:
+        # expected arguments: script path, dataset path, output file path
+        print('ERR: Expected two arguments: (input file path, output file path).')
+        exit(1)
+
+    # read program arguments
+    run_time_params = dict()
+    run_time_params['app_name'] = 'hw3-task2_2'
+    run_time_params['in_dir'] = sys.argv[1]
+    run_time_params['test_file'] = sys.argv[2]
+    run_time_params['out_file'] = sys.argv[3]
+    run_time_params['train'] = 'yelp_train.csv'
+    run_time_params['user'] = 'user.json'
+    run_time_params['business'] = 'business.json'
+    return run_time_params
+
+
+def parse_train_set():
+    filename = '{}/{}'.format(params['in_dir'], params['train'])
+    with open(filename, 'r') as fh:
+        header = fh.readline().strip()
+
+    return sc.textFile(filename) \
+        .filter(lambda line: line.strip() != header) \
+        .map(lambda line: line.split(',')) \
+        .map(lambda record: (record[0], record[1], float(record[2])))
+
+
+def parse_user_set():
+    filename = '{}/{}'.format(params['in_dir'], params['user'])
+
+    return sc.textFile(filename) \
+        .map(lambda json_string: json.loads(json_string)) \
+        .map(lambda user_obj: (user_obj['user_id'], (user_obj['review_count'],
+                               user_obj['useful'], user_obj['funny'], user_obj['cool'], user_obj['fans'],
+                               user_obj['average_stars']
+                               ))
+             )
+
+
+def parse_business_set():
+    filename = '{}/{}'.format(params['in_dir'], params['business'])
+
+    return sc.textFile(filename) \
+        .map(lambda json_string: json.loads(json_string)) \
+        .map(lambda business_obj: (business_obj['business_id'], (business_obj['stars'], business_obj['review_count'])))
+
+
+def write_results_to_file(data):
+    file_header = 'user_id, business_id, rating, review_count, useful, funny,' \
+                  ' cool, fans, average_stars, business_stars, business_review_count\n'
+    with open(params['out_file'], 'w') as fh:
+        fh.write(file_header)
+        for record in data:
+            fh.write('{}\n'.format(','.join(map(lambda item: str(item), list(record)))))
+
+
+def fill_features(record, user_data, business_data):
+    user_features = user_data.get(record[0], (0, 0, 0, 0, 0, 0, 0))
+    business_features = business_data.get(record[1], (0, 0))
+    return record + user_features + business_features
+
+
+def main():
+    user_data = parse_user_set().collectAsMap()
+    business_data = parse_business_set().collectAsMap()
+
+    train_set = parse_train_set() \
+        .map(lambda record: fill_features(record, user_data, business_data)) \
+        .collect()
+
+    write_results_to_file(train_set)
+
+    train_df = df = pd.DataFrame(train_set, columns=['user_id', 'business_id', 'rating', 'review_count', 'useful',
+                                                     'funny', 'cool', 'fans', 'average_stars', 'business_stars',
+                                                     'business_review_count'
+                                                     ]
+                                 )
+
+    model = xgb.XGBRegressor(n_estimators=1000, colsample_bytree=0.9, learning_rate=0.01)
+    model.fit(train_df.drop(['user_id', 'business_id', 'rating'], axis=1).values, train_df[['rating']].values)
+
+
+if __name__ == '__main__':
+    # set executables
+    os.environ['PYSPARK_PYTHON'] = sys.executable
+    os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
+    # initialize program parameters
+    params = parse_args()
+
+    # create spark context
+    sc = SparkContext(conf=SparkConf().setAppName(params['app_name']).setMaster("local[*]"))
+    sc.setLogLevel('ERROR')
+
+    # run prediction
+    start_ts = datetime.now()
+    main()
+    end_ts = datetime.now()
+    print('Duration: ', (end_ts - start_ts).total_seconds())
+
+    # exit without errors
+    exit(0)
