3ada6cd882f2ada9b472424b1c6c9e17214371a3
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-04-06 00:00:43 -0700

    implement girvan-newman algorithm

--- homework-assignment-4/python/task2.py
+++ homework-assignment-4/python/task2.py
@@ -32,7 +32,8 @@ def get_runtime_params():
         filter_threshold=int(sys.argv[1]),
         in_file=sys.argv[2],
         out_betweenness_file=sys.argv[3],
-        out_modularity_file=sys.argv[4]
+        out_modularity_file=sys.argv[4],
+        precision=5
     )
 
 
@@ -53,20 +54,79 @@ def get_edges_from_dataset():
             .map(lambda pair: (pair[0][0], pair[1][0]))
 
 
+def get_level_tree(graph_al, root):
+    level_tree = defaultdict(lambda: defaultdict(set))
+    bf_tree = defaultdict(int)
+    bf_tree[root] = 1
+    level_tree[0] = {root: set()}
+    q = [(root, 1)]
+    introduced = defaultdict(lambda: float('inf'))
+    introduced[root] = 0
+    visited = set()
+    while len(q) > 0:
+        node, level = q.pop(0)
+        if node not in visited:
+            visited.add(node)
+            for neighbor in graph_al[node]:
+                if neighbor not in visited:
+                    q.append((neighbor, level + 1))
+                    if level < introduced[neighbor]:
+                        level_tree[level][neighbor].add(node)
+                        bf_tree[neighbor] += bf_tree[node]
+                        introduced[neighbor] = level + 1
+
+    return level_tree, bf_tree
+
+
+def get_edge_betweenness(bf_tree, level_tree):
+    edge_betweenness = defaultdict(int)
+    node_credits = defaultdict(int)
+    for level in range(max(level_tree.keys()), -1, -1):
+        for node, parents in level_tree[level].items():
+            total_credits = 1 + node_credits[node]
+            total_paths = reduce(
+                lambda acc, parent_node: acc + bf_tree[parent_node],
+                parents,
+                0
+            )
+            for parent in parents:
+                credits = total_credits * (bf_tree[parent] / total_paths)
+                edge_betweenness[tuple(sorted([node, parent]))] = credits
+                node_credits[parent] += credits
+
+    return edge_betweenness
+
+
+def girvan_newman(edges_rdd):
+    graph_al = edges_rdd \
+        .groupByKey() \
+        .mapValues(set) \
+        .collectAsMap()
+    edge_betweenness = defaultdict(int)
+    for node in graph_al:
+        level_tree, bf_tree = get_level_tree(graph_al, node)
+        for edge, betweenness in get_edge_betweenness(bf_tree, level_tree).items():
+            edge_betweenness[edge] += betweenness
+
+    for edge in edge_betweenness:
+        edge_betweenness[edge] /= 2
+
+    return edge_betweenness
+
+
+def write_betweenness_to_file(edge_betweenness):
+    with open(params.out_betweenness_file, 'w') as fh:
+        for edge, betweenness in sorted(edge_betweenness.items(), key=lambda c: (c[1], c[0][0]), reverse=True):
+            fh.write('{},{}\n'.format(edge, round(betweenness, params.precision)))
+
+
 def main():
     # get edges - all users with a certain number of common businesses
-    all_edges = get_edges_from_dataset()
-    edges = all_edges \
-        .map(lambda pair: tuple(sorted(pair))) \
-        .distinct()
+    edges_rdd = get_edges_from_dataset()
 
-    # get vertices - all unique users
-    vertices = all_edges \
-        .map(lambda user_pair: user_pair[0]) \
-        .distinct()
-
-    print(edges.collect())
-    print(vertices.collect())
+    # task 2 part 1 - get betweenness values for the original graph and save the output to the file
+    betweenness = girvan_newman(edges_rdd)
+    write_betweenness_to_file(betweenness)
 
 
 if __name__ == '__main__':
