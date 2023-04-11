44daf1b7c2af530728d8616787d2ad3cf33256e5
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-04-11 11:50:38 -0700

    add - spark init for hw5 task 1

--- homework-assignment-5/python/task1.py
+++ homework-assignment-5/python/task1.py
@@ -5,10 +5,12 @@ Task 1
 Implement a bloom filter on a simulated datastream.
 """
 
-
+import os
 import sys
 
 from argparse import Namespace
+from datetime import datetime
+from pyspark import SparkConf, SparkContext
 
 
 def get_runtime_params():
@@ -18,6 +20,7 @@ def get_runtime_params():
 
     # return the params
     return Namespace(
+        APP_NAME='hw5-task1',
         INPUT_FILE=sys.argv[1],
         STREAM_SIZE=sys.argv[2],
         NUM_OF_ASKS=sys.argv[3],
@@ -25,7 +28,27 @@ def get_runtime_params():
     )
 
 
+def main():
+    pass
+
+
 if __name__ == '__main__':
+    # set executables
+    os.environ['PYSPARK_PYTHON'] = sys.executable
+    os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
     # get runtime params
     PARAMS = get_runtime_params()
-    print(PARAMS)
+
+    # create spark context
+    sc = SparkContext(conf=SparkConf().setAppName(PARAMS.APP_NAME).setMaster("local[*]"))
+    sc.setLogLevel('ERROR')
+
+    # run community detection (based on Label Propagation Algorithm
+    start_ts = datetime.now()
+    main()
+    end_ts = datetime.now()
+    print('Duration: ', (end_ts - start_ts).total_seconds())
+
+    # exit without errors
+    exit(0)
