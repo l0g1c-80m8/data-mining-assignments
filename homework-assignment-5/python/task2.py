06738cae53e352837a8fd0e0a143283b58692ba4
Rutvik Patel <rutvik.p4t3l@gmail.com>
2023-04-12 17:01:06 -0700

    generate user hashes in binary strings

--- homework-assignment-5/python/task2.py
+++ homework-assignment-5/python/task2.py
@@ -2,7 +2,7 @@
 Homework Assignment 5
 Task 1
 
-Implement Flajolet-Martin algorithm to estimate the number of unique users in a window of data steam.
+Implement Fljolet-Martin algorithm to estimate the number of unique users in a window of data steam.
 """
 
 import sys
@@ -10,12 +10,28 @@ import sys
 from argparse import Namespace
 from binascii import hexlify
 from blackbox import BlackBox  # local import
+from collections import namedtuple
 from datetime import datetime
 from functools import reduce
+from random import randint
 
 
 def construct_hashers():
-    return list()
+    HasherParams = namedtuple('HasherParams', ['a', 'b', 'm'])
+    return list(map(
+        lambda hash_params:
+        lambda x: format(
+            ((hash_params.a * x + hash_params.b) % PARAMS.PRIME_MODULUS) % hash_params.m,
+            '0{}b'.format(PARAMS.BIN_HASH_LEN)
+        ),
+        reduce(
+            lambda hashers, _: hashers + [
+                HasherParams(a=randint(1, sys.maxsize), b=randint(0, sys.maxsize), m=randint(0, sys.maxsize))
+            ],
+            range(PARAMS.N_HASHERS),
+            list()
+        )
+    ))
 
 
 def get_runtime_params():
@@ -31,7 +47,16 @@ def get_runtime_params():
         NUM_OF_ASKS=int(sys.argv[3]),
         OUTPUT_FILE=sys.argv[4],
         N_HASHERS=10,
+        BIN_HASH_LEN=64,
+        PRIME_MODULUS=4213398913,  # randomly generated prime in range 1 billion to 10 billion
         OUT_FILE_HEADERS=['Time', 'Ground Truth', 'Estimation']
+    )
+
+
+def get_runtime_global_ns():
+    return Namespace(
+        BX=BlackBox(),
+        HASHERS=construct_hashers()
     )
 
 
@@ -49,7 +74,7 @@ def get_user_hashes(user):
     user_int_hash = int(hexlify(user.encode('utf8')), 16)
     return reduce(
         lambda user_hashes, hash_func: user_hashes + [hash_func(user_int_hash)],
-        HASHERS,
+        GLOB_NS.HASHERS,
         list()
     )
 
@@ -64,7 +89,9 @@ def write_results_to_file(results):
 def main():
     results = list()
 
-    # exec Flajolet-Martin here
+    for run_idx_ in range(PARAMS.NUM_OF_ASKS):
+        for user in GLOB_NS.BX.ask(PARAMS.INPUT_FILE, PARAMS.STREAM_SIZE):
+            print(get_user_hashes(user))
 
     # write the results to a file
     write_results_to_file(results)
@@ -74,11 +101,8 @@ if __name__ == '__main__':
     # get runtime params
     PARAMS = get_runtime_params()
 
-    # construct hashers
-    HASHERS = construct_hashers()
-
-    # init obj of BlackBox to generate streams
-    bx = BlackBox()
+    # define a namespace for global variables
+    GLOB_NS = get_runtime_global_ns()
 
     # run community detection (based on Label Propagation Algorithm
     start_ts = datetime.now()
