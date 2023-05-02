65d8d3cfd7708901cd989452536558dff269c171
rutvik patel <rutvik.p4t3l@gmail.com>
2023-05-02 10:16:41 -0700

    update params ns from dict to Namespace + 1

--- Competition Project/competition.py
+++ Competition Project/competition.py
@@ -33,7 +33,7 @@ def parse_args():
 
 
 def parse_train_set():
-    filename = '{}/{}'.format(PARAMS.IN_DIR, PARAMS.TRAIN_FILE)
+    filename = '{}/{}'.format(PARAMS_NS.IN_DIR, PARAMS_NS.TRAIN_FILE)
     with open(filename, 'r') as fh:
         header = fh.readline().strip()
 
@@ -44,64 +44,64 @@ def parse_train_set():
 
 
 def parse_test_set():
-    with open(PARAMS.TEST_FILE, 'r') as fh:
+    with open(PARAMS_NS.TEST_FILE, 'r') as fh:
         header = fh.readline().strip()
 
-    return sc.textFile(PARAMS.TEST_FILE) \
+    return sc.textFile(PARAMS_NS.TEST_FILE) \
         .filter(lambda line: line.strip() != header) \
         .map(lambda line: line.split(',')) \
         .map(lambda record: (record[0], record[1]))
 
 
 def parse_val_set():
-    with open(PARAMS.TEST_FILE, 'r') as fh:
+    with open(PARAMS_NS.TEST_FILE, 'r') as fh:
         header = fh.readline().strip()
 
-    return sc.textFile(PARAMS.TEST_FILE) \
+    return sc.textFile(PARAMS_NS.TEST_FILE) \
         .filter(lambda line: line.strip() != header) \
         .map(lambda line: line.split(',')) \
         .map(lambda record: ((record[0], record[1]), float(record[2])))
 
 
 def parse_user_set():
-    filename = '{}/{}'.format(PARAMS.IN_DIR, PARAMS.USER_FILE)
+    filename = '{}/{}'.format(PARAMS_NS.IN_DIR, PARAMS_NS.USER_FILE)
 
     return sc.textFile(filename) \
         .map(lambda json_string: loads(json_string)) \
-        .map(lambda user_obj: (user_obj[PARAMS.RECORD_COLS[0]],
+        .map(lambda user_obj: (user_obj[PARAMS_NS.RECORD_COLS[0]],
                                tuple(map(
                                    lambda col_name: user_obj[col_name],
-                                   PARAMS.USER_FEATURE_COLS
+                                   PARAMS_NS.USER_FEATURE_COLS
                                ))
                                )
              )
 
 
 def parse_business_set():
-    filename = '{}/{}'.format(PARAMS.IN_DIR, PARAMS.BUSINESS_FILE)
+    filename = '{}/{}'.format(PARAMS_NS.IN_DIR, PARAMS_NS.BUSINESS_FILE)
 
     return sc.textFile(filename) \
         .map(lambda json_string: loads(json_string)) \
-        .map(lambda business_obj: (business_obj[PARAMS.RECORD_COLS[1]],
+        .map(lambda business_obj: (business_obj[PARAMS_NS.RECORD_COLS[1]],
                                    tuple(map(
                                        lambda col_name: business_obj[col_name.replace('business_', '')],
-                                       PARAMS.BUSINESS_FEATURE_COLS
+                                       PARAMS_NS.BUSINESS_FEATURE_COLS
                                    ))
                                    )
              )
 
 
 def write_results_to_file(data):
-    file_header = '{}\n'.format(', '.join(PARAMS.RECORD_COLS))
-    with open(PARAMS.OUT_FILE, 'w') as fh:
+    file_header = '{}\n'.format(', '.join(PARAMS_NS.RECORD_COLS))
+    with open(PARAMS_NS.OUT_FILE, 'w') as fh:
         fh.write(file_header)
         for record in data:
             fh.write('{}\n'.format(','.join(map(lambda item: str(item), list(record)))))
 
 
 def fill_features(record, user_data, business_data):
-    user_features = user_data.get(record[0], tuple([0] * len(PARAMS.USER_FEATURE_COLS)))
-    business_features = business_data.get(record[1], tuple([0] * len(PARAMS.BUSINESS_FEATURE_COLS)))
+    user_features = user_data.get(record[0], tuple([0] * len(PARAMS_NS.USER_FEATURE_COLS)))
+    business_features = business_data.get(record[1], tuple([0] * len(PARAMS_NS.BUSINESS_FEATURE_COLS)))
     return record + user_features + business_features
 
 
@@ -137,11 +137,9 @@ def log_results(study):
     print('Number of finished trials: {}'.format(len(study.trials)))
     print('Best trial:')
     trial = study.best_trial
-
     print('  Value: {}'.format(trial.value))
     print('  Params: ')
-
-    for key, value in trial.PARAMS.items():
+    for key, value in trial.PARAMS_NS.items():
         print('    {}: {}'.format(key, value))
 
 
@@ -187,7 +185,7 @@ def main():
         parse_train_set()
         .map(lambda record: fill_features(record, user_data, business_data))
         .collect(),
-        columns=PARAMS.RECORD_COLS + PARAMS.USER_FEATURE_COLS + PARAMS.BUSINESS_FEATURE_COLS
+        columns=PARAMS_NS.RECORD_COLS + PARAMS_NS.USER_FEATURE_COLS + PARAMS_NS.BUSINESS_FEATURE_COLS
     )
 
     # create test dataset
@@ -195,9 +193,8 @@ def main():
         .map(lambda record: fill_features(record, user_data, business_data)) \
         .collect()
     test_df = DataFrame(test_set,
-                           columns=PARAMS.RECORD_COLS[: -1] + PARAMS.USER_FEATURE_COLS + PARAMS[
-                               'business_feature_cols']
-                           )
+                        columns=PARAMS_NS.RECORD_COLS[: -1] + PARAMS_NS.USER_FEATURE_COLS + PARAMS_NS.BUSINESS_FEATURE_COLS
+                        )
 
     # format data and write to file
     # write_results_to_file(map(
@@ -205,14 +202,13 @@ def main():
     #     enumerate(test_set)
     # ))
 
-    x_train, y_train = train_df.drop(PARAMS.RECORD_COLS, axis=1).values, \
-        train_df[PARAMS.RECORD_COLS[-1:]].values
-    x_test = test_df.drop(PARAMS.RECORD_COLS[: -1], axis=1).values
+    x_train, y_train = train_df.drop(PARAMS_NS.RECORD_COLS, axis=1).values, \
+        train_df[PARAMS_NS.RECORD_COLS[-1:]].values
+    x_test = test_df.drop(PARAMS_NS.RECORD_COLS[: -1], axis=1).values
     validations = parse_val_set().collectAsMap()
 
     study = create_study(direction='minimize')
     study.optimize(_objective, n_trials=1)
-
     log_results(study)
 
 
@@ -222,10 +218,10 @@ if __name__ == '__main__':
     environ['PYSPARK_DRIVER_PYTHON'] = executable
 
     # initialize program parameters
-    PARAMS = parse_args()
+    PARAMS_NS = parse_args()
 
     # create spark context
-    sc = SparkContext(conf=SparkConf().setAppName(PARAMS.APP_NAME).setMaster("local[*]"))
+    sc = SparkContext(conf=SparkConf().setAppName(PARAMS_NS.APP_NAME).setMaster("local[*]"))
     sc.setLogLevel('ERROR')
 
     # run prediction
